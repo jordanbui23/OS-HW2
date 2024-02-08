d1cec8d9edb59c2d2af3adeccc3cb2e50d93a364
Jordan Bui <stajordansta@gmail.com>
2024-02-08 17:42:30 -0600

    Initial Commit

---
+++ src/process_scheduling.c
@@ -0,0 +1,61 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "dyn_array.h"
+#include "processing_scheduling.h"
+
+
+// You might find this handy.  I put it around unused parameters, but you should
+// remove it before you submit. Just allows things to compile initially.
+#define UNUSED(x) (void)(x)
+
+// private function
+void virtual_cpu(ProcessControlBlock_t *process_control_block) 
+{
+    // decrement the burst time of the pcb
+    --process_control_block->remaining_burst_time;
+}
+
+bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
+    UNUSED(ready_queue);
+    UNUSED(result);
+    return false;
+}
+
+bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
+    UNUSED(ready_queue);
+    UNUSED(result);
+    return false;   
+}
+
+bool priority(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
+    UNUSED(ready_queue);
+    UNUSED(result);
+    return false;   
+}
+
+bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quantum) 
+{
+    UNUSED(ready_queue);
+    UNUSED(result);
+    UNUSED(quantum);
+    return false;
+}
+
+dyn_array_t *load_process_control_blocks(const char *input_file) 
+{
+    UNUSED(input_file);
+    return NULL;
+}
+
+bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
+    UNUSED(ready_queue);
+    UNUSED(result);
+    return false;
+}
